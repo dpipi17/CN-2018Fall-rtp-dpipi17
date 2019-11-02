d637329075c54429580e4cc3a337143558eb62b2
dpipi17 <48618124+dpipi17@users.noreply.github.com>
2019-11-02 20:34:45 +0400

    finished

--- Sender.py
+++ Sender.py
@@ -16,7 +16,115 @@ class Sender(BasicSender.BasicSender):
     # Main sending loop.
     def start(self):
       # add things here
-      pass
+
+        # HandShake
+        firstAck = b''
+        for _ in range(0,60):
+            
+            hello = self.make_packet('syn', 0, '')
+            self.send(hello)
+            firstAck = self.receive(1)
+            
+            if firstAck is None:
+                continue
+
+            break
+    
+        if not firstAck:
+            return
+        
+
+        # Split Packet
+        seqNum = 1
+        data = self.infile.read()
+        dataDict = dict()
+
+        key = 1
+        currIndex = 0
+        chunkSize = 1400
+        while True:     
+            currData = b''
+            if currIndex + chunkSize < len(data):
+                dataDict[key] = (data[currIndex : currIndex + chunkSize], False)
+            else:
+                dataDict[key] = (data[currIndex:], False)
+                break
+            
+            currIndex += chunkSize
+            key += 1
+        
+        
+        # Send Data
+        while True:
+            if seqNum > key:
+                break
+            
+            for packetInd in range(seqNum, min(seqNum + 7, key) + 1):
+                if not dataDict[packetInd][1]:
+                    currPacketDat = dataDict[packetInd][0]
+                    message = self.make_packet('dat', packetInd, currPacketDat)
+                    
+                    self.send(message)
+
+            recievedMessages = []
+            counterDict = dict()
+            flag = False
+
+            while True: 
+                
+                if flag:
+                    break
+
+                recievedMessage = self.receive(0.5)
+                if recievedMessage is None:
+                    break
+
+                if Checksum.validate_checksum(recievedMessage):
+                    splittedPac = self.split_packet(recievedMessage)
+                    recievedMessages.append(splittedPac)
+
+
+                    if splittedPac[0] == 'ack':
+                        pacNum = splittedPac[1]
+                        if pacNum in counterDict:
+                            counterDict[pacNum] = counterDict[pacNum] + 1
+                        else:
+                            counterDict[pacNum] = 1
+
+                        if counterDict[pacNum] >= 4:
+                            seqNum = int(pacNum)
+                            flag = True
+
+            if flag:
+                continue
+            
+            for msg in recievedMessages:
+                if msg[0] == 'sack':
+                    selAckArr = msg[1].split(';')
+
+                    sentPackets = []
+                    seqNum = max(seqNum, int(selAckArr[0]))
+                    
+                    if len(selAckArr) > 1 and selAckArr[1] != '':
+                        selAckArrSecond = selAckArr[1].split(',')
+
+                        for ack in selAckArrSecond:
+                            if ack != '':
+                                sentPackets.append(int(ack))
+
+                        for pacInd in sentPackets:
+                            dataDict[pacInd] = (dataDict[pacInd][0], True)
+
+                else:
+                    seqNum = max(seqNum, int(msg[1]))
+                
+             
+        # say goodbye
+        bye = self.make_packet('fin', seqNum, '')
+        self.send(bye)
+
+
+
         
 '''
 This will be run if you run this script from the command line. You should not
